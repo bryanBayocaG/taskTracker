3e00b119b53955c91f995c95e49fe40bfdc3672b
Bryan Adanza Bayoca <brynbayoca686@gmail.com>
2025-04-12 10:34:38 +0800

    passing props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponentComponent } from './header-component/header-component.component';
 import { UserComponentComponent } from './user-component/user-component.component';
+import { TaskComponent } from './task/task.component';
 import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponentComponent, UserComponentComponent],
+  imports: [HeaderComponentComponent, UserComponentComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-udemy';
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
   select(id: string) {
-    console.log('User selected: ' + id);
+    this.selectedUserId = id;
   }
 }
