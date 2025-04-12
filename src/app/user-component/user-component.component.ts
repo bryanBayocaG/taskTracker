c85e74efb86b86025c37983b6a00b493056b08b5
Bryan Adanza Bayoca <brynbayoca686@gmail.com>
2025-04-12 11:23:15 +0800

    for loop and if statements

--- src/app/user-component/user-component.component.ts
+++ src/app/user-component/user-component.component.ts
@@ -7,14 +7,16 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   styleUrl: './user-component.component.css',
 })
 export class UserComponentComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: {
+    id: string;
+    avatar: string;
+    name: string;
+  };
   @Output() select = new EventEmitter();
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
