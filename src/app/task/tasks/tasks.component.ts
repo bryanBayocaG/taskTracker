73d2166ea606984355aae59924634d6d653fd1e4
Bryan Adanza Bayoca <brynbayoca686@gmail.com>
2025-04-14 11:49:41 +0800

    complete task

--- src/app/task/tasks/tasks.component.ts
+++ src/app/task/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 interface Task {
   id: string;
@@ -16,4 +16,9 @@ interface Task {
 })
 export class TasksComponent {
   @Input({ required: true }) task!: Task;
+  @Output() completeTask = new EventEmitter<string>();
+
+  onCompleteTask() {
+    this.completeTask.emit(this.task.id);
+  }
 }
